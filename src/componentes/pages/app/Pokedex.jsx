fff5466e09ef714d778e9c21f96cbb606263f333
Nek92 <86691957+Nek92@users.noreply.github.com>
2022-04-17 19:44:07 -0300

    cambios

--- src/componentes/pages/app/Pokedex.jsx
+++ src/componentes/pages/app/Pokedex.jsx
@@ -1,41 +1,37 @@
 import "./App.css";
-import { useState, useEffect } from 'react';
-import { TYPES } from '../equipo-pokemon/Accion' ;
+import { useState, useEffect } from "react";
 const pokemons_numbers = 150;
 
 function Pokedex() {
-  const [list, setList] = useState([])
-
+  const [list, setList] = useState([]);
 
   const FetchPokemons = async () => {
-    const gottaFetchenAll = []
+    const gottaFetchenAll = [];
     for (let i = 1; i <= pokemons_numbers; i++) {
-      gottaFetchenAll.push(await getPokemon(i))
+      gottaFetchenAll.push(await getPokemon(i));
     }
 
-    await setList(gottaFetchenAll)
-  }
+    await setList(gottaFetchenAll);
+  };
 
   const getPokemon = async (id) => {
-    const url = "https://pokeapi.co/api/v2/pokemon/" + id.toString()
-    const res = await fetch(url)
-    return (await res.json())
-  }
-
+    const url = "https://pokeapi.co/api/v2/pokemon/" + id.toString();
+    const res = await fetch(url);
+    return await res.json();
+  };
 
   useEffect(() => {
-    FetchPokemons()
-  }, [])
+    FetchPokemons();
+  }, []);
 
-  useEffect(async () => {
-  }, [list])
+  useEffect(async () => {}, [list]);
 
-const Agregar = (id) => {}
+  const Agregar = (id) => {};
 
   const CreatePokemonCard = (pokemon) => {
-    const { name, types, sprites, id } = pokemon
-    const type = types[0].type.name
-    return (  
+    const { name, types, sprites, id } = pokemon;
+    const type = types[0].type.name;
+    return (
       <div className="pokemon grow">
         <div className="img_container">
           <img src={sprites.front_default} alt={name}></img>
@@ -44,17 +40,18 @@ const Agregar = (id) => {}
         <span className="number">{id}</span>
         <h3 className="name">{name}</h3>
         <small className="type">{type}</small>
+        <button onClick>Agregar a Equipo</button>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div className="App">
       <h1>PokeNek</h1>
-      <div id="poke_container" className="poke_container"> </div>
-      {
-        list.map(pokemon => CreatePokemonCard(pokemon))
-      }
+      <div id="poke_container" className="poke_container">
+        {" "}
+      </div>
+      {list.map((pokemon) => CreatePokemonCard(pokemon))}
     </div>
   );
 }
--- src/componentes/pages/equipo-pokemon/Accion.js
+++
@@ -1,6 +0,0 @@
-export const TYPES = {
-    Agregar : "Agregar" ,
-    Eliminar : "Eliminar"
-}
-
-export default TYPES;
